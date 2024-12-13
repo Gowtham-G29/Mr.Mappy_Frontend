a10ffbe54cc1119a0a5819b05135e66ba95a03a3
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-14 03:41:50 +0530

    update : chunk size for build

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,16 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist', // This should point to the correct directory
+  }, rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes('node_modules')) {
+          return 'vendor';  // Creates a separate 'vendor' chunk for node_modules
+        }
+      },
+    },
   },
+  chunkSizeWarningLimit: 1000, // Increases the warning limit to 1000 KB (1 MB)
+
 })
 
